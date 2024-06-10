e7d86b4611b5bf35e2fc16af227c393cea6bd6b5
fajarsaputttra <fajar_saputra@teknokrat.ac.id>
2024-06-10 10:46:36 +0700

    add PUT & Delete all table

--- connection.js
+++ connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 
 //Variable konesksi untuk database
 const db = mysql.createConnection ({
-    host:'sql6.freesqldatabase.com',
-    user:'sql6695735',
-    database:'sql6695735',
-    password:'yAFC7Lu2yN'
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_otomotif',
 })
 
 //kirimkan variable keluar untuk digunakan diluar file
--- index.js
+++ index.js
@@ -1,125 +1,214 @@
-//Basic Routing express
 const express = require('express')
 const app = express()
 const port = 3001
-//Pemanggil Request body parse (Ternyarta Parse dan Parser berbeda! becarefull install)
-const bodyParser = require('body-parser') 
-//Pemanggil file connection.js di index.js
+const bodyParser = require('body-parser')
 const db = require('./connection.js')
-//Pemanggil file response.js
 const response = require('./request.js')
 
-//Membuat Penggunaan body parser 
 app.use(bodyParser.json())
 
-//Membuat Route Get Data tb_pelanggan
-app.get('/pelanggan',(req,res)=>{
-    const sql = 'SELECT * From tb_pelanggan'
-    db.query(sql,(error,result)=>{
-        response(200,result,'data pelanggan',res)
+//PELANGGAN
+// Route Get Data tb_pelanggan
+app.get('/pelanggan', (req, res) => {
+    const sql = 'SELECT * FROM tb_pelanggan'
+    db.query(sql, (error, result) => {
+        response(200, result, 'data pelanggan', res)
     })
 })
 
-//Membuat Route Detail Get Data tb_pelanggan
-app.get('/pelanggan/:id',(req,res)=>{
+// Route Detail Get Data tb_pelanggan
+app.get('/pelanggan/:id', (req, res) => {
     const id = req.params.id
-    const sql = `SELECT * From tb_pelanggan where id_pelanggan ='${id}' `
-    db.query(sql,(err,result)=>{
+    const sql = `SELECT * FROM tb_pelanggan WHERE id_pelanggan ='${id}'`
+    db.query(sql, (err, result) => {
         if (err) throw err
-        response(200,result,"get detail pelanggan",res)
+        response(200, result, "get detail pelanggan", res)
     })
 })
 
-//Membuat Route Get Data tb_produk
-app.get('/produk',(req,res)=>{
-    const sql = 'SELECT * From tb_produk'
-    db.query(sql,(error,result)=>{
-        response(200,result,'data produk',res)
-    })
-})
+// Route Post tb_pelanggan
+app.post('/pelanggan', (req, res) => {
+    const { id, nama, alamat, jenis_member } = req.body
+    const sql = `INSERT INTO tb_pelanggan (id_pelanggan, nama, alamat, jenis_member) VALUES ('${id}', '${nama}', '${alamat}', '${jenis_member}')`
 
-//Membuat Route Detail Get Data tb_produk
-app.get('/produk/:id',(req,res)=>{
-    const id = req.params.id
-    const sql = `SELECT * From tb_produk where id_produk ='${id}' `
-    db.query(sql,(err,result)=>{
-        if (err) throw err
-        response(200,result,"get detail produk",res)
-    })
-})
-
-//Membuat Route Get Data tb_transaksi
-app.get('/transaksi',(req,res)=>{
-    const sql = 'SELECT * From tb_transaksi'
-    db.query(sql,(error,result)=>{
-        response(200,result,'data transaksi',res)
-    })
-})
-//Membuat Route Detail Get Data tb_transaksi
-app.get('/transaksi/:id',(req,res)=>{
-    const id = req.params.id
-    const sql = `SELECT * From tb_transaksi where id_transaksi ='${id}' `
-    db.query(sql,(err,result)=>{
-        if (err) throw err
-        response(200,result,"get detail transaksi",res)
-    })
-})
-
-//Membuat Route Post tb_pelanggan
-app.post('/pelanggan', (req,res)=>{
-    const {id, nama, alamat, jenis_member}=req.body
-    const sql = `INSERT INTO tb_pelanggan (id_pelanggan, nama, alamat, jenis_member) values ('${id}','${nama}','${alamat}','${jenis_member}')`
-
-    db.query(sql,(error,fields)=>{
-        if(error) response(500, 'invalid', `pelanggan ${nama} dengan jenis member ${jenis_member} sudah ditambahkan`,res)
-        if(fields?.affectedRows){
-            const data ={
-                isSuccess:fields.affectedRows,
-                id:fields.insertId,
+    db.query(sql, (error, fields) => {
+        if (error) response(500, 'invalid', `pelanggan ${nama} dengan jenis member ${jenis_member} sudah ditambahkan`, res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId,
             }
-            response(200,data,"Data berhasil disimpan",res)
+            response(200, data, "Data berhasil disimpan", res)
         }
     })
-
 })
 
-//Membuat Route Post tb_produk
-app.post('/produk', (req,res)=>{
-    const {id, nama, harga, stok}=req.body
-    const sql = `INSERT INTO tb_produk (id_produk, nama_produk, harga, stok) values ('${id}','${nama}','${harga}','${stok}');`
+// Route PUT tb_pelanggan
+app.put('/pelanggan/edit/:id', (req, res) => {
+    const id = req.params.id
+    const { nama, alamat, jenis_member } = req.body
+    const sql = `UPDATE tb_pelanggan SET nama='${nama}', alamat='${alamat}', jenis_member='${jenis_member}' WHERE id_pelanggan='${id}'`
 
-    db.query(sql,(error,fields)=>{
-        if(error) response(500, 'invalid', `produk ${nama} dengan harga ${harga} sudah ditambahkan`,res)
-        if(fields?.affectedRows){
-            const data ={
-                isSuccess:fields.affectedRows,
-                id:fields.insertId,
-            }
-            response(200,data,"Data berhasil disimpan",res)
+    db.query(sql, (error, result) => {
+        if (error) response(500, 'invalid', `Error updating pelanggan ${id}`, res)
+        if (result.affectedRows) {
+            response(200, { isSuccess: result.affectedRows }, "Data berhasil diupdate", res)
+        } else {
+            response(404, null, "Pelanggan tidak ditemukan", res)
         }
     })
-
 })
 
-//Membuat Route Post tb_transaksi
-app.post('/transaksi', (req,res)=>{
-    const {id_transaksi,id_pelanggan,id_produk, tanggal_transaksi, jumlah_barang, total_harga}=req.body
-    const sql = `INSERT INTO tb_transaksi (id_transaksi, id_pelanggan, id_produk, tanggal_transaksi,jumlah_barang,total_harga) 
-                values ('${id_transaksi}','${id_pelanggan}','${id_produk}','${tanggal_transaksi}','${jumlah_barang}','${total_harga}');`
+// Route DELETE tb_pelanggan
+app.delete('/pelanggan/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `DELETE FROM tb_pelanggan WHERE id_pelanggan='${id}'`
 
-    db.query(sql,(error,fields)=>{
-        if(error) response(500, 'invalid', `produk ${nama} dengan harga ${harga} sudah ditambahkan`,res)
-        if(fields?.affectedRows){
-            const data ={
-                isSuccess:fields.affectedRows,
-                id:fields.insertId,
-            }
-            response(200,data,"Data berhasil disimpan",res)
+    db.query(sql, (error, result) => {
+        if (error) response(500, 'invalid', `Error deleting pelanggan ${id}`, res)
+        if (result.affectedRows) {
+            response(200, { isSuccess: result.affectedRows }, "Data berhasil dihapus", res)
+        } else {
+            response(404, null, "Pelanggan tidak ditemukan", res)
         }
     })
-
 })
 
-app.listen(port,()=>{
-    console.log(`Runing in port ${port}`)
+//PRODUK
+// Route Get Data tb_produk
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM tb_produk'
+    db.query(sql, (error, result) => {
+        response(200, result, 'data produk', res)
+    })
+})
+
+// Route Detail Get Data tb_produk
+app.get('/produk/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `SELECT * FROM tb_produk WHERE id_produk ='${id}'`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        response(200, result, "get detail produk", res)
+    })
+})
+
+// Route Post tb_produk
+app.post('/produk', (req, res) => {
+    const { id, nama, harga, stok } = req.body
+    const sql = `INSERT INTO tb_produk (id_produk, nama_produk, harga, stok) VALUES ('${id}', '${nama}', '${harga}', '${stok}')`
+
+    db.query(sql, (error, fields) => {
+        if (error) response(500, 'invalid', `produk ${nama} dengan harga ${harga} sudah ditambahkan`, res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId,
+            }
+            response(200, data, "Data berhasil disimpan", res)
+        }
+    })
+})
+
+// Route PUT tb_produk
+app.put('/produk/edit/:id', (req, res) => {
+    const id = req.params.id
+    const { nama_produk, harga, stok } = req.body
+    const sql = `UPDATE tb_produk SET nama_produk='${nama_produk}', harga='${harga}', stok='${stok}' WHERE id_produk='${id}'`
+
+    db.query(sql, (error, result) => {
+        if (error) response(500, 'invalid', `Error updating produk ${id}`, res)
+        if (result.affectedRows) {
+            response(200, { isSuccess: result.affectedRows }, "Data berhasil diupdate", res)
+        } else {
+            response(404, null, "Produk tidak ditemukan", res)
+        }
+    })
+})
+
+// Route DELETE tb_produk
+app.delete('/produk/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `DELETE FROM tb_produk WHERE id_produk='${id}'`
+
+    db.query(sql, (error, result) => {
+        if (error) response(500, 'invalid', `Error deleting produk ${id}`, res)
+        if (result.affectedRows) {
+            response(200, { isSuccess: result.affectedRows }, "Data berhasil dihapus", res)
+        } else {
+            response(404, null, "Produk tidak ditemukan", res)
+        }
+    })
+})
+
+//TRANSAKSI
+// Route Get Data tb_transaksi
+app.get('/transaksi', (req, res) => {
+    const sql = 'SELECT * FROM tb_transaksi'
+    db.query(sql, (error, result) => {
+        response(200, result, 'data transaksi', res)
+    })
+})
+
+// Route Detail Get Data tb_transaksi
+app.get('/transaksi/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `SELECT * FROM tb_transaksi WHERE id_transaksi ='${id}'`
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        response(200, result, "get detail transaksi", res)
+    })
+})
+
+// Route Post tb_transaksi
+app.post('/transaksi', (req, res) => {
+    const { id_transaksi, id_pelanggan, id_produk, tanggal_transaksi, jumlah_barang, total_harga } = req.body
+    const sql = `INSERT INTO tb_transaksi (id_transaksi, id_pelanggan, id_produk, tanggal_transaksi, jumlah_barang, total_harga) 
+                VALUES ('${id_transaksi}', '${id_pelanggan}', '${id_produk}', '${tanggal_transaksi}', '${jumlah_barang}', '${total_harga}')`
+
+    db.query(sql, (error, fields) => {
+        if (error) response(500, 'invalid', `produk ${nama} dengan harga ${harga} sudah ditambahkan`, res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId,
+            }
+            response(200, data, "Data berhasil disimpan", res)
+        }
+    })
+})
+
+// Route PUT tb_transaksi
+app.put('/transaksi/edit/:id', (req, res) => {
+    const id = req.params.id
+    const { id_pelanggan, id_produk, tanggal_transaksi, jumlah_barang, total_harga } = req.body
+    const sql = `UPDATE tb_transaksi SET id_pelanggan='${id_pelanggan}', id_produk='${id_produk}', tanggal_transaksi='${tanggal_transaksi}', jumlah_barang='${jumlah_barang}', total_harga='${total_harga}' WHERE id_transaksi='${id}'`
+
+    db.query(sql, (error, result) => {
+        if (error) response(500, 'invalid', `Error updating transaksi ${id}`, res)
+        if (result.affectedRows) {
+            response(200, { isSuccess: result.affectedRows }, "Data berhasil diupdate", res)
+        } else {
+            response(404, null, "Transaksi tidak ditemukan", res)
+        }
+    })
+})
+
+// Route DELETE tb_transaksi
+app.delete('/transaksi/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `DELETE FROM tb_transaksi WHERE id_transaksi='${id}'`
+
+    db.query(sql, (error, result) => {
+        if (error) response(500, 'invalid', `Error deleting transaksi ${id}`, res)
+        if (result.affectedRows) {
+            response(200, { isSuccess: result.affectedRows }, "Data berhasil dihapus", res)
+        } else {
+            response(404, null, "Transaksi tidak ditemukan", res)
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Running in port ${port}`)
 })
